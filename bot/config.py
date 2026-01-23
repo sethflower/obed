5d59a42e8d5992c9a112bf19ef11fa116ef4aa04
sethflower <139699611+sethflower@users.noreply.github.com>
2026-01-23 17:49:17 +0200

    Update BOT_TOKEN in config.py
    Replaced default BOT_TOKEN with a specific token.

--- bot/config.py
+++ bot/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8372459093:AAEfF6Ev2cOoKjxMW3kZLvh3oYoMm5OFIXs")
 DB_PATH = os.getenv("DB_PATH", "data.sqlite3")
